9ae75322a245307f5966127bc8f517bc8042fcdf
gabrielbeckmann-debug <gabriel.beckmann0018@gmail.com>
2026-05-07 17:37:54 +0200

    Refactor comments and enhance welcome message creation
    Updated comments for clarity and added a note about the welcome message file.

--- create_users.sh
+++ create_users.sh
@@ -13,7 +13,7 @@ if test $# -eq 0; then
   exit 1
 fi
 
-#Här börjar själva for loopen med lista som lagrar användare som läggs in som argument. Listan skapar ett home directory för varje användare och skapar sedan nödvändiga mappar. Därefter ändras ägarskapet av mappen till användaren och rwx rättigheter sätts efteråt.
+#Här börjar själva for loopen med lista som lagrar användare som läggs in som argument. Listan skapar ett home directory för varje användare och skapar sedan nödvändiga underliggande mappar. Därefter ändras ägarskapet av mappen till användaren och rwx rättigheter sätts efteråt. Till slut skapas en .txt fil med välkomstmeddelande
 for user in "$@"; do
   useradd -m $user
   mkdir /home/$user/Documents
